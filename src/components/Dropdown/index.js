4803c75cb3741ae064527bb9e4ba0161041eceb7
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-09-02 17:20:09 +0200

    Changed URL

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -2,19 +2,19 @@ import React from 'react'
 import Link from 'next/link'
 import styles from '../Dropdown/Dropdown.module.scss'
 
-const Dropdown = ({ options, name }) => {
+const Dropdown = ({ options, name, dataTable }) => {
   return (
     <div className={styles.dropdown} aria-label="Dropdown items">
       <ul className={styles.dropdown__menu}>
         <li className={styles.dropdown__item} aria-label={`all ${name}`}>
-          <Link href={`/[group]`} as={`/${name}`}>
+          <Link href={`/[group]`} as={`/${dataTable}`}>
             <a className={styles.dropdown__link}>{`all ${name}`}</a>
           </Link>
         </li>
         {options.map((opt, i) => {
           return (
             <li key={i} className={styles.dropdown__item} aria-label={opt}>
-              <Link href={`/[group]/[type]`} as={`/${name}/${opt.id}`}>
+              <Link href={`/[group]/[type]`} as={`/${dataTable}/${opt.id}`}>
                 <a className={styles.dropdown__link}>{opt.name}</a>
               </Link>
             </li>

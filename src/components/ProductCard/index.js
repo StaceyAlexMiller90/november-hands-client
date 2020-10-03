e9f73ca05c5c4bb5abedc247da366fe3c8a42e02
StaceyAlexMiller90 <staceyalexmiller@gmail.com>
2020-10-03 15:06:45 +0200

    Add product detail page with link

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,21 +1,25 @@
 import React from 'react'
-
+import Link from 'next/link'
 import styles from './ProductCard.module.scss'
 
 const ProductCard = ({ product }) => {
-  const { productName, price, color, images, discount, discountedPrice } = product
+  const { productColorId, productName, price, color, images, discount, discountedPrice } = product
 
   return (
-    <article className={styles.product_card}>
-      <figure className={styles.product_card__figure}>
-        <img src={images[0]} className={styles.product_card__image}></img>
-      </figure>
-      <header className={styles.product_card__header}>
-        <h3 className={styles.product_card__title}>{`${productName} - ${color}`}</h3>
-        <p className={`${styles.product_card__price} ${discount && styles.product_card__price_discounted}`}>€{price}</p>
-        {discount && <p className={styles.product_card__price}>€{discountedPrice}</p>}
-      </header>
-    </article>
+    <Link href={`/products/[id]`} as={`/products/${productColorId}`}>
+      <article className={styles.product_card}>
+        <figure className={styles.product_card__figure}>
+          <img src={images[0]} className={styles.product_card__image}></img>
+        </figure>
+        <header className={styles.product_card__header}>
+          <h3 className={styles.product_card__title}>{`${productName} - ${color}`}</h3>
+          <p className={`${styles.product_card__price} ${discount && styles.product_card__price_discounted}`}>
+            €{price}
+          </p>
+          {discount && <p className={styles.product_card__price}>€{discountedPrice}</p>}
+        </header>
+      </article>
+    </Link>
   )
 }
 
